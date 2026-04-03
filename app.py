67c16db5c71ff0e96e23c2577971384d22333a52
maureliocostadev <70759015+maureliocostadev@users.noreply.github.com>
2026-04-03 08:55:34 -0300

    Update saldo_atual_mp and user data entries
    Updated the current balance and modified data entries for some users.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 from datetime import datetime
 
 
-
+saldo_atual_mp = 1169.59
 # -----------------------------
 # CONFIGURAÇÃO DA PÁGINA
 # -----------------------------
@@ -96,7 +96,7 @@ st.markdown("""
 
 
 
-saldo_atual_mp = 918.71
+
 # -----------------------------
 # DADOS (MVP)
 # -----------------------------
@@ -105,9 +105,9 @@ data = [
     {"Nome": "Delzuita", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 0, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
     {"Nome": "Edmundo", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 0, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
     {"Nome": "Fabiana", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 0, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
-    {"Nome": "Marcos", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 0, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
-    {"Nome": "Kellianny", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 0, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
-    {"Nome": "Maria Socorro", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 0, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
+    {"Nome": "Marcos", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 82, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
+    {"Nome": "Kellianny", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 82, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
+    {"Nome": "Maria Socorro", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 82, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
     {"Nome": "Marisa", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 0, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
     {"Nome": "João Vitor", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 0, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
     {"Nome": "Katarine", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 0, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
