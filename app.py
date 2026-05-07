f19d4033d5369626b669ee7605431f539eea6c95
maureliocostadev <70759015+maureliocostadev@users.noreply.github.com>
2026-05-06 21:51:35 -0300

    Update data entries for Delzuita and Edmundo

--- app.py
+++ app.py
@@ -102,8 +102,8 @@ st.markdown("""
 # -----------------------------
 data = [
     {"Nome": "Davi", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 82, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
-    {"Nome": "Delzuita", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 0, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
-    {"Nome": "Edmundo", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 0, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
+    {"Nome": "Delzuita", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 82, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
+    {"Nome": "Edmundo", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 82, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
     {"Nome": "Fabiana", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 82, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
     {"Nome": "Marcos", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 82, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
     {"Nome": "Kellianny", "Janeiro": 10, "Fevereiro": 26, "Março": 55, "Abril": 82, "Maio": 0, "Junho": 0, "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0},
